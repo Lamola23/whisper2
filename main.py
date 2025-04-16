c6d4febf4354bad80bcd9ffcba09beabe1f0e2cc
Lamola23 <Monicaortega0023@gmail.com>
2025-04-16 17:06:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from fastapi import FastAPI, UploadFile, File
-from faster_whisper import WhisperModel
-import tempfile
+import whisper
+import shutil
+import os
+print("🚀 FastAPI is starting up...")
 
 app = FastAPI()
-model = WhisperModel("base")
+
 
 @app.post("/transcribe")
 async def transcribe(file: UploadFile = File(...)):
-    with tempfile.NamedTemporaryFile(delete=True) as tmp:
-        tmp.write(await file.read())
-        tmp.flush()
-        segments, _ = model.transcribe(tmp.name)
-        transcription = "".join([segment.text for segment in segments])
-    return {"transcription": transcription}
+    model = whisper.load_model("tiny")
+    temp_path = f"temp_{file.filename}"
+    
+    with open(temp_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    result = model.transcribe(temp_path)
+    os.remove(temp_path)
+
+    return {"text": result["text"]}
